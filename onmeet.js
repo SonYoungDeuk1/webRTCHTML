22d2a2a79d1575c8bd1292820bc6824c8b1eeb9d
SonYoungDeuk1 <deukzzang@gmail.com>
2022-02-18 10:22:04 +0900

    Update onmeet.js

--- onmeet.js
+++ onmeet.js
@@ -6256,8 +6256,8 @@ function afterSessionCreationSuccess() {
     // });
     $('#details').remove();
     $('#dashboard').removeClass('hide').show();
-    $('#roomcontainer').hide();
-    $("#configModal").hide();
+    //$('#roomcontainer').hide();
+    //$("#configModal").hide();
     getUserList();
     getConfRoomList()
 }
